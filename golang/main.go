66925c3225be9d84fc75ef7f9c592c37c82ccd50
kpenfound <kyle@dagger.io>
2023-09-13 18:14:36 -0400

    fix return types

--- golang/main.go
+++ golang/main.go
@@ -7,30 +7,30 @@ import (
 
 type Golang struct {}
 
-func (m *Golang) Base(ctx context.Context, version string) (*Container) {
+func (m *Golang) Base(ctx context.Context, version string) (*Container, error) {
 	cache := dag.CacheVolume("gomodcache")
 	image := fmt.Sprintf("golang:%s", version)
 	return dag.Container().
 	From(image).
 	WithMountedCache("/gomodcache", cache).
-	WithEnvironmentVariable("GOMODCACHE", "/gomodcache")
+	WithEnvVariable("GOMODCACHE", "/gomodcache").Sync(ctx)
 }
 
 func (c *Container) GoBuild(ctx context.Context, args []string) (*Container, error) {
-	command := append([]string{"go", "build"}, args)
+	command := append([]string{"go", "build"}, args...)
 	return c.WithExec(command).Sync(ctx)
 }
 
 func (c *Container) GoTest(ctx context.Context, ctr *Container, args []string) (*Container, error) {
-	command := append([]string{"go", "test"}, args)
+	command := append([]string{"go", "test"}, args...)
 	return c.WithExec(command).Sync(ctx)
 }
 
-//func (d *Directory) GoLint(ctx context.Context) (string, error) {
-//	return dag.Container().From("golangci/golangci-lint:v1.48").
-//		WithMountedDirectory("/src", d).
-//		WithWorkdir("/src").
-//		WithExec([]string{"golangci-lint", "run", "-v", "--timeout", "5m"}).
-//		Stdout(ctx)
-//}
+func (d *Directory) GoLint(ctx context.Context) (string, error) {
+	return dag.Container().From("golangci/golangci-lint:v1.48").
+		WithMountedDirectory("/src", d).
+		WithWorkdir("/src").
+		WithExec([]string{"golangci-lint", "run", "-v", "--timeout", "5m"}).
+		Stdout(ctx)
+}
 
