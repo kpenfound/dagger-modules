ed236283ff22da75cbab2182d50b0d3f3a0715da
kpenfound <kyle@dagger.io>
2023-09-22 13:43:00 -0400

    vault module

---
+++ vault/go.mod
@@ -0,0 +1,22 @@
+module vault
+
+go 1.21.1
+
+require (
+	github.com/99designs/gqlgen v0.17.31
+	github.com/Khan/genqlient v0.6.0
+	github.com/hashicorp/vault-client-go v0.4.0
+	golang.org/x/sync v0.3.0
+)
+
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
---
+++ vault/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	vcg "github.com/hashicorp/vault-client-go"
+	"github.com/hashicorp/vault-client-go/schema"
+)
+
+type Vault struct {
+	ApproleID     string
+	ApproleSecret string
+	Address       string
+}
+
+func (v *Vault) Auth(approleID, approleSecret, address string) *Vault {
+	v.ApproleID = approleID
+	v.ApproleSecret = approleSecret
+	v.Address = address
+
+	return v
+}
+
+func (v *Vault) GetSecret(ctx context.Context, secret, key string) (string, error) {
+	client, err := getClient(ctx, v)
+	if err != nil {
+		return "", err
+	}
+	s, err := client.Secrets.KvV2Read(ctx, secret, vcg.WithMountPath("secret"))
+	if err != nil {
+		return "", err
+	}
+	return s.Data.Data[key]
+}
+
+func (v *Vault) PutSecret(ctx context.Context, secret, key, value string) (*Vault, error) {
+	client, err := getClient(ctx, v)
+	if err != nil {
+		return nil, err
+	}
+	_, err = client.Secrets.KvV2Write(ctx, secret, schema.KvV2WriteRequest{
+		Data: map[string]any{
+			key: value,
+		}},
+		vcg.WithMountPath("secret"),
+	)
+	return v, err
+}
+
+func getClient(ctx context.Context, v *Vault) (*vcg.Client, error) {
+	client, err := vcg.New(
+		vcg.WithAddress(v.Address),
+		vcg.WithRequestTimeout(30*time.Second),
+	)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := client.Auth.AppRoleLogin(
+		ctx,
+		schema.AppRoleLoginRequest{
+			RoleId:   v.ApproleID,
+			SecretId: v.ApproleSecret,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.SetToken(resp.Auth.ClientToken); err != nil {
+		return nil, err
+	}
+	return client, nil
+}
