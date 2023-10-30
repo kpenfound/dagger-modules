751d9b4f64e0d49dbc11e3106060e6be62b0db8c
Kyle Penfound <kyle@dagger.io>
2023-10-30 16:53:47 -0400

    init fly module
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

---
+++ fly/src/main.py
@@ -0,0 +1,17 @@
+import dagger
+from dagger.mod import function
+
+@function
+def deploy(image: str, port: int, token: dagger.Secret) -> str:
+    return (
+        fly_base(token)
+        .with_exec(["fly", "deploy", "--image", image, "--internal-port", port])
+    )
+
+def fly_base(token: dagger.Secret) -> dagger.Container:
+    return (
+        dagger.container().from_("alpine:latest")
+        .with_exec(["sh", "-c", "curl -L https://fly.io/install.sh | sh"])
+        .with_secret_variable("FLY_API_TOKEN", token)
+    )
+
