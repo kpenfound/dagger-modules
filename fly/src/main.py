d0aefc20303c951bed6d1d53daa0035b40d03f0f
Kyle Penfound <kyle@dagger.io>
2023-10-30 17:18:10 -0400

    install curl
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- fly/src/main.py
+++ fly/src/main.py
@@ -11,6 +11,7 @@ def deploy(image: str, port: int, token: dagger.Secret) -> str:
 def fly_base(token: dagger.Secret) -> dagger.Container:
     return (
         dagger.container().from_("alpine:latest")
+        .with_exec(["apk", "add", "curl"])
         .with_exec(["sh", "-c", "curl -L https://fly.io/install.sh | sh"])
         .with_secret_variable("FLY_API_TOKEN", token)
     )
