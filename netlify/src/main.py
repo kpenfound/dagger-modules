9e98d4b6c88a7ce63109ff21a51786b4bf3dc26f
Kyle Penfound <kyle@dagger.io>
2023-10-30 14:35:20 -0400

    pass site
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- netlify/src/main.py
+++ netlify/src/main.py
@@ -6,34 +6,33 @@ CLI = "netlify-cli@16.9.3"
 @function
 def deploy(dir: dagger.Directory, token: dagger.Secret, site: str) -> str:
     return (
-        netlify_base(token, site)
+        netlify_base(token)
         .with_mounted_directory("/src", dir)
-        .with_exec(["netlify", "deploy", "--dir", "/src", "--prod"])
+        .with_exec(["netlify", "deploy", "--dir", "/src", "--site", site, "--prod"])
         .stdout()
     )
 
 @function
 def preview(dir: dagger.Directory, token: dagger.Secret, site: str) -> str:
     return (
-        netlify_base(token, site)
+        netlify_base(token)
         .with_mounted_directory("/src", dir)
-        .with_exec(["netlify", "deploy", "--dir", "/src"])
+        .with_exec(["netlify", "deploy", "--dir", "/src", "--site", site])
         .stdout()
     )
 
 @function
 def list(token: dagger.Secret) -> str:
     return (
-        netlify_base(token, "")
+        netlify_base(token)
         .with_exec(["netlify", "sites:list"])
         .stdout()
     )
 
 
-def netlify_base(token: dagger.Secret, site: str) -> dagger.Container:
+def netlify_base(token: dagger.Secret) -> dagger.Container:
     return (
         dagger.container().from_("node:21-alpine")
         .with_exec(["npm", "install", "-g", CLI]).with_entrypoint([])
         .with_secret_variable("NETLIFY_AUTH_TOKEN", token)
-        .with_env_variable("NETLIFY_SITE_ID", site)
     )
