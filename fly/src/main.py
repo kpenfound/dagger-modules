1562764fd00c0125331fdef239f6621f5a0fe6d9
Kyle Penfound <kyle@dagger.io>
2023-10-30 22:10:58 -0400

    template fly.toml
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- fly/src/main.py
+++ fly/src/main.py
@@ -3,9 +3,24 @@ from dagger.mod import function
 
 @function
 def deploy(app: str, image: str, token: dagger.Secret) -> str:
+    config = f'''
+app = "{app}"
+primary_region = "ord"
+
+[build]
+  image = "{image}"
+
+[http_service]
+  internal_port = 8080
+  force_https = true
+  auto_stop_machines = true
+  auto_start_machines = true
+  min_machines_running = 0
+    '''
     return (
         fly_base(token)
-        .with_exec(["/root/.fly/bin/flyctl", "deploy", "--app", app, "--image", image])
+        .with_new_file("/fly.toml", config) # TODO: make more of these things options
+        .with_exec(["/root/.fly/bin/flyctl", "deploy", "--config", "/fly.toml"])
     )
 
 def fly_base(token: dagger.Secret) -> dagger.Container:
