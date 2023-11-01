478da8a41d6ca6c9af7697e9fad202c50509ca17
Kyle Penfound <kyle@dagger.io>
2023-10-31 20:58:32 -0400

    fix foo
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- foo/src/main.py
+++ foo/src/main.py
@@ -4,12 +4,12 @@ from dagger.mod import function, object_type
 @object_type
 class Foo:
     @function
-    def container_echo(string_arg: str) -> dagger.Container:
+    def container_echo(self, string_arg: str) -> dagger.Container:
         # Example usage: "dagger call container-echo --string-arg hello"
         return dagger.container().from_("alpine:latest").with_exec(["echo", string_arg])
 
     @function
-    async def grep_dir(directory_arg: dagger.Directory, pattern: str) -> str:
+    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
         # Example usage: "dagger call grep-dir --directory-arg . --patern grep_dir"
         return await (
             dagger.container()
