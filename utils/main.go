16db9cf49ff47759954ff84d9f76be92f8b3ca21
Kyle Penfound <kyle@dagger.io>
2023-12-11 14:59:14 -0500

    utils fixes
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- utils/main.go
+++ utils/main.go
@@ -5,17 +5,18 @@ import (
 	"fmt"
 )
 
-type Utils struct {}
+type Utils struct{}
 
 // Get a tarball of a Directory
 func (m *Utils) Tar(dir *Directory) *File {
-	ctr := dag.Container().
-	From("alpine:3.18").
-	WithMountedDirectory("/assets", dir).
-	WithExec([]string{"tar", "czf", "out.tar.gz", "/assets"}).
-	File("out.tar.gz")
+	return dag.Container().
+		From("alpine:3.18").
+		WithMountedDirectory("/assets", dir).
+		WithExec([]string{"tar", "czf", "out.tar.gz", "/assets"}).
+		File("out.tar.gz")
 }
 
+// Concurrently Sync multiple Containers
 func (m *Utils) Multisync(ctx context.Context, ctrs []*Container) error {
 	hck := dag.Directory()
 
@@ -25,7 +26,7 @@ func (m *Utils) Multisync(ctx context.Context, ctrs []*Container) error {
 		hck = hck.WithFile(ctrFile, ctr.File("/syncfile"))
 	}
 
-	_, err := hck.Entries()
+	_, err := hck.Entries(ctx)
 
 	return err
 }
