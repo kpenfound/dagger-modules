de7fcc1e4fb290e67c3f1ec6b8a290e0fb1156eb
Kyle Penfound <kyle@dagger.io>
2024-02-08 10:43:23 -0500

    update ruff image
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- ruff/src/main.py
+++ ruff/src/main.py
@@ -5,7 +5,7 @@ from dagger import dag, function
 async def check(directory: dagger.Directory) -> str:
     return await (
         dag.container()
-        .from_("pipeline-components/ruff:latest")
+        .from_("ghcr.io/astral-sh/ruff")
         .with_mounted_directory("/src", directory)
         .with_workdir("/src")
         .with_exec(["ruff", "check", "."])
