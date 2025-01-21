bbaa67a1e33bb5588187514fc1bcdfe1d6980a33
kpenfound <kyle@dagger.io>
2025-01-21 11:06:27 -0500

    generate
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ ollama/main.go
@@ -0,0 +1,25 @@
+// A module for Ollama and Dagger
+
+package main
+
+import (
+	"context"
+	"dagger/ollama/internal/dagger"
+)
+
+type Ollama struct{}
+
+// Returns a container that echoes whatever string argument is provided
+func (m *Ollama) ContainerEcho(stringArg string) *dagger.Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *Ollama) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+	return dag.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/mnt", directoryArg).
+		WithWorkdir("/mnt").
+		WithExec([]string{"grep", "-R", pattern, "."}).
+		Stdout(ctx)
+}
